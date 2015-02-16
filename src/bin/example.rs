0a48c10330cfbb3e7b60d05b16a23b6556732748
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-02-16 07:54:22 +0300

    Add simple example

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,11 +1,27 @@
+#![feature(io)]
+#![feature(env)]
+#![feature(path)]
+
 extern crate demon;
 use demon::Demon;
 use demon::DemonRunner;
+use std::env;
+use std::old_io::{File, Open, ReadWrite, IoError, SeekStyle};
 
 fn main() {
+	log("Example started.");
 	let demon = Demon {
 		name: "Example".to_string()
 	};
 	demon.run();
+	log("Example finished.");
 }
 
+
+fn log(message: &str) -> Result<(), IoError> {
+	let path = try! (env::current_exe()).with_extension("log");
+	let mut file = try! (File::open_mode(&path, Open, ReadWrite));
+	try! (file.seek(0, SeekStyle::SeekEnd));
+	try! (file.write_line(message));
+	Ok(())
+}
