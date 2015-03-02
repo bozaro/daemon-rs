f0ddebac745f0bdf69503382d36c780f33e123b3
Artem V. Navrotskiy <bozaro@yandex.ru>
2015-03-02 22:49:09 +0300

    Fix compilation

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -1,3 +1,4 @@
+#![feature(fs)]
 #![feature(io)]
 #![feature(env)]
 #![feature(path)]
@@ -7,7 +8,9 @@ use demon::Signal;
 use demon::Demon;
 use demon::DemonRunner;
 use std::env;
-use std::old_io::{File, Open, ReadWrite, IoError, SeekStyle};
+use std::fs::OpenOptions;
+use std::io::Error;
+use std::io::Write;
 use std::sync::mpsc::Receiver;
 
 fn main() {
@@ -35,11 +38,12 @@ fn log(message: &str) {
 	log_safe(message);
 }
 
-fn log_safe(message: &str) -> Result<(), IoError> {
+fn log_safe(message: &str) -> Result<(), Error> {
 	let path = try! (env::current_exe()).with_extension("log");
-	let mut file = try! (File::open_mode(&path, Open, ReadWrite));
-	try! (file.seek(0, SeekStyle::SeekEnd));
-	try! (file.write_line(message));
+	let mut file = try! (OpenOptions::new().append(true).open(&path));
+//	try! (file.seek(0, SeekStyle::SeekEnd));
+	try! (file.write(message.as_bytes()));
+	try! (file.write(b"\n"));
 	println! ("{}", message);
 	Ok(())
 }
