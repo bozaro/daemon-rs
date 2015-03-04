056327fe24c0791e0a55aaa18b261c059b6b2848
a.navrotskiy <navrotskiy@corp.mail.ru>
2015-03-04 16:00:36 +0300

    Refactoring

--- src/windows.rs
+++ src/windows.rs
@@ -22,45 +22,51 @@ struct DemonStatic
 
 impl DemonRunner for Demon
 {
-	fn run<F: FnOnce(Receiver<State>)>(&self, f: F) {
+	fn run<F: FnOnce(Receiver<State>)>(&self, func: F) -> Result<(), String> {
 		let (tx, rx) = channel();
 		tx.send(State::Start).unwrap();
 		let mut demon = DemonStatic
 		{
 			tx: Some(tx),
 		};
-
-		unsafe
-		{
-			let guard = LOCK.lock().unwrap();
-			if demon_static != demon_null()
-			{
-				panic! ("This function is not reentrant.");
-			}
-			demon_static = &mut demon;
-			let _ = guard;
-		}
 		
-		unsafe
-		{
-			if SetConsoleCtrlHandler(Some(console_handler), TRUE) == FALSE
-			{
-				panic! ("SetConsoleCtrlHandler: {}", error_string(GetLastError() as i32));
-			}
-			f(rx);
-			if SetConsoleCtrlHandler(Some(console_handler), FALSE) == FALSE
-			{
-				panic! ("SetConsoleCtrlHandler: {}", error_string(GetLastError() as i32));
-			}
-		}
+		try! (guard_compare_and_swap(demon_null(), &mut demon));
+		let result = demon_console(func, rx);
+		try! (guard_compare_and_swap(&mut demon, demon_null()));
+		result
+	}
+}
 
-		unsafe
+fn guard_compare_and_swap(old_value: *mut DemonStatic, new_value: *mut DemonStatic) -> Result<(), String>
+{
+	unsafe
+	{
+		let guard = LOCK.lock().unwrap();
+		if demon_static != old_value
 		{
-			let guard = LOCK.lock().unwrap();
-			demon_static = demon_null();
-			let _ = guard;
+			return Err("This function is not reentrant.".to_string());
+		}
+		demon_static = new_value;
+		let _ = guard;
+	}
+	Ok(())
+}
+
+fn demon_console<F: FnOnce(Receiver<State>)>(func: F, rx: Receiver<State>) -> Result<(), String>
+{
+	unsafe
+	{
+		if SetConsoleCtrlHandler(Some(console_handler), TRUE) == FALSE
+		{
+			return Err(format! ("Failed SetConsoleCtrlHandler: {}", error_string(GetLastError() as i32)));
+		}
+		func(rx);
+		if SetConsoleCtrlHandler(Some(console_handler), FALSE) == FALSE
+		{
+			return Err(format! ("Failed SetConsoleCtrlHandler: {}", error_string(GetLastError() as i32)));
 		}
 	}
+	Ok(())
 }
 
 unsafe extern "system" fn console_handler(_: DWORD) -> BOOL
