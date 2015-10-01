f83d221a425c1c9494398e72fefbe866799dbe26
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2015-10-01 21:26:32 +0200

    Support OS X

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub mod windows;
 #[cfg(target_os = "windows")]
 pub use windows::*;
 
-#[cfg(target_os = "linux")]
-pub mod linux;
-#[cfg(target_os = "linux")]
-pub use linux::*;
+#[cfg(any(target_os = "linux", target_os="macos"))]
+pub mod posix;
+#[cfg(any(target_os = "linux", target_os="macos"))]
+pub use posix::*;
